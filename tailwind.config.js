4c5cfb9d6756c16dad4c67f9f71d1ac99cd48899
violeteverg <me@violeteverg.com>
2023-08-14 22:43:49 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        raleway: ["Raleway", "sans-serif"],
+        pacifico: ["Pacifico", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
